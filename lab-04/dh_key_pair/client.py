65a3d1a8e8dde1eb607552ac2804a60c0ede37fc
Tran Nguyen Bao Dang <trannguyenbaodang2005@gmail.com>
2026-03-25 14:32:36 +0700

    [add] lab-04 tornado websocket

---
+++ lab-04/dh_key_pair/client.py
@@ -0,0 +1,31 @@
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives import serialization
+
+
+def generate_client_key_pair(parameters):
+    private_key = parameters.generate_private_key()
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+
+def derive_shared_secret(private_key, server_public_key):
+    shared_key = private_key.exchange(server_public_key)
+    return shared_key
+
+
+def main():
+    # Load server public key
+    with open("server_public_key.pem", "rb") as f:
+        server_public_key = serialization.load_pem_public_key(f.read())
+
+    parameters = server_public_key.parameters()
+
+    private_key, public_key = generate_client_key_pair(parameters)
+
+    shared_secret = derive_shared_secret(private_key, server_public_key)
+
+    print("Shared Secret:", shared_secret.hex())
+
+
+if __name__ == "__main__":
+    main()
---
+++ lab-04/hash/sha-256.py
@@ -0,0 +1,13 @@
+import hashlib
+
+
+def calculate_sha256_hash(data):
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(data.encode('utf-8'))  # Chuyển dữ liệu thành bytes
+    return sha256_hash.hexdigest()  # Trả về biểu diễn hex của hash
+
+
+data_to_hash = input("Nhập dữ liệu để hash bằng SHA-256: ")
+hash_value = calculate_sha256_hash(data_to_hash)
+
+print("Giá trị hash SHA-256:", hash_value)
